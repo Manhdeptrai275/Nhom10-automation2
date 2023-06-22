f0cb35e332c0210366924698aa4c563716dc3c66
admin <ducmanhhoangthieu@gmail.com>
2023-06-22 19:43:04 +0700

    addting files

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,6 +3,8 @@ package org.example;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
@@ -25,11 +27,63 @@ public class Main {
         WebElement menu = driver.findElement(By.xpath("//button[text()='Open Menu']"));
         menu.click();
 
-        WebElement sort = driver.findElement(By.className("product_sort_container"));
-        sort.click();
+        WebElement sortDropdown = driver.findElement(By.className("product_sort_container"));
+        Select sortSelect = new Select(sortDropdown);
+        sortSelect.selectByValue("lohi");
 
         WebElement item = driver.findElement(By.xpath("//div[@class='inventory_item_desc']"));
         System.out.println(item.getText());
+        // thêm 2 sp đầu tiên
+        WebElement addToCartButton1 = driver.findElement(By.xpath("//div[@class='inventory_item'][1]//button"));
+        WebElement addToCartButton2 = driver.findElement(By.xpath("//div[@class='inventory_item'][2]//button"));
+        addToCartButton1.click();
+        addToCartButton2.click();
+//Chọn giỏ hàng
+        WebElement cartButton = driver.findElement(By.className("shopping_cart_container"));
+        cartButton.click();
+//in giỏ hàng
+        WebElement cartItems = driver.findElement(By.className("cart_list"));
+        System.out.println("Thông tin giỏ hàng:");
+        System.out.println(cartItems.getText());
+//        remove sản phẩm
+        WebElement removeButton2 = driver.findElement(By.xpath("//div[@class='cart_item'][2]//button[text()='Remove']"));
+        removeButton2.click();
+//        continue
+        WebElement continueShoppingButton = driver.findElement(By.id("continue-shopping"));
+        continueShoppingButton.click();
+//        thếmsp
+        WebElement addToCartButton = driver.findElement(By.xpath("//div[@class='inventory_item'][1]//button"));
+        addToCartButton.click();
+//        Chọn giỏ hàng
+        cartButton = driver.findElement(By.className("shopping_cart_container"));
+        cartButton.click();
+//        click ckeckout
+        WebElement checkoutButton = driver.findElement(By.id("checkout"));
+        checkoutButton.click();
+//        thông tin bất kì
+        WebElement firstNameInput = driver.findElement(By.id("first-name"));
+        WebElement lastNameInput = driver.findElement(By.id("last-name"));
+        WebElement zipCodeInput = driver.findElement(By.id("postal-code"));
+
+        firstNameInput.sendKeys("John");
+        lastNameInput.sendKeys("Doe");
+        zipCodeInput.sendKeys("12345");
+//        Click continue và lưu lại thông tin
+        WebElement continueButton = driver.findElement(By.xpath("//input[@value='Continue']"));
+        continueButton.click();
+        String orderSummary = driver.findElement(By.className("summary_info")).getText();
+        System.out.println("Thông tin đơn hàng:");
+        System.out.println(orderSummary);
+//        Logout
+        WebElement menuButton = driver.findElement(By.xpath("//button[@id='react-burger-menu-btn']"));
+        menuButton.click();
+        WebElement logoutLink = driver.findElement(By.xpath("//a[text()='Logout']"));
+        logoutLink.click();
+
+
+
+
+
 
     }
 }
